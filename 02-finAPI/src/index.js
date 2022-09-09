c5fdd314ffbb0b46f321f2cdb2e4498c19abe581
julia <juliabiersuriano@gmail.com>
2022-09-09 01:31:26 -0400

    create customer function

--- 02-finAPI/src/index.js
+++ 02-finAPI/src/index.js
@@ -3,6 +3,8 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
+app.use(express.json());
+
 const customers = [];
 
 /*
@@ -14,12 +16,16 @@ const customers = [];
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
-    const id = uuidv4();
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({'error': 'Customer already exists'})
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: [],
     });
 
