ecd272d25ded0c0c57f74e38a80018d34ac1fc69
julia <juliabiersuriano@gmail.com>
2022-09-12 15:42:39 -0400

    added make withdraw route

--- 02-finAPI/README.md
+++ 02-finAPI/README.md
@@ -3,7 +3,7 @@ finAPI - Finance system
 ## REQUIREMENTS
 - [X] Deve ser possível criar uma conta
 - [X] Deve ser possível buscar o extrato do cliente
-- [ ] Deve ser possível realizar um depósito
+- [X] Deve ser possível realizar um depósito
 - [ ] Deve ser possível realizar um saque
 - [ ] Deve ser possível buscar o extrato do cliente por data
 - [ ] Deve ser possível atualizar os dados da conta do cliente
@@ -13,7 +13,7 @@ finAPI - Finance system
 
 ## BUSINESS RULE
 - [X] Não deve ser possível cadastrar uma conta com CPF já existente
-- [ ] Não deve ser possível fazer depósito em uma conta não existente
+- [X] Não deve ser possível fazer depósito em uma conta não existente
 - [X] Não deve ser possível buscar extrato em uma conta não existente
 - [ ] Não deve ser possível fazer saque em uma conta não existente
 - [ ] Não deve ser possível excluir uma conta não existente
--- 02-finAPI/src/index.js
+++ 02-finAPI/src/index.js
@@ -22,6 +22,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
 
@@ -56,6 +68,28 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
         amount,
         created_at: new Date(),
         type: 'credit',
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (amount > balance) {
+        return response.status(400).json({'error': 'Insufficient funds'})
+    }
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'debit',
     };
 
     customer.statement.push(statementOperation);
