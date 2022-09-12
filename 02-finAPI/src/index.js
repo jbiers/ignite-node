02f54248a7d5f66c6f76d4523bcf83013d474bad
julia <juliabiersuriano@gmail.com>
2022-09-12 15:32:41 -0400

    created deposit route

--- 02-finAPI/src/index.js
+++ 02-finAPI/src/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -7,12 +8,20 @@ app.use(express.json());
 
 const customers = [];
 
-/*
- *  cpf - string
- *  name - string
- *  id - uuid
- *  statement - []
- */
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(404).json({'error': 'Customer not found'})
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
 
@@ -32,16 +41,26 @@ app.post('/account', (request, response) => {
     return response.status(201).send();
 });
 
-app.get('/statement/', (request, response) => {
-    const { cpf } = request.headers;
-
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if (!customer) {
-        return response.status(404).json({'error': 'Customer not found'})
-    }
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
 
     return response.status(200).json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'credit',
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
 });
 
 app.listen(3333);
